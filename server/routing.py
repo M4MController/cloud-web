5ef71298cf804b1c0a5b98cbca9e8c94bbcdf2f0
Yaxo-O <aaazzzx@mail.ru>
2019-05-27 20:11:23 +0100

    feature/objects-info: /objects returns objects from db

--- server/routing.py
+++ server/routing.py
@@ -1,28 +1,10 @@
 from server.config import config
 from server.resources.base import BaseResource
-from marshmallow import Schema, fields
+from server.database.schemas import SensorDataSchema, ResourceSchema
 
 from server.resources.utils import provide_db_session, schematic_response
 
-from server.database.managers import SensorManager, SensorDataManager
-
-
-class SensorDataSchema(Schema):
-    time_stamp = fields.DateTime(attribute='timestamp')
-    value = fields.Dict()
-
-
-class SensorSchema(Schema):
-    id = fields.Integer()
-    name = fields.String()
-    status = fields.Integer()
-    last_value = fields.Dict(allow_none=True)
-    type = fields.Integer()
-    controller = fields.Integer(attribute='controller_id')
-
-
-class ResourceSchema(Schema):
-    sensors = fields.Nested(SensorSchema(), many=True)
+from server.database.managers import SensorManager, SensorDataManager, ObjectManager, ControllerManager
 
 
 class Auth(BaseResource):
@@ -47,9 +29,13 @@ class ObjectsResource(BaseResource):
     @schematic_response(ResourceSchema())
     def get(self):
         sensors = SensorManager(self.db_session).get_all()
+        objects = ObjectManager(self.db_session).get_all()
+        controllers = ControllerManager(self.db_session).get_all()
         self._insert_last_value(sensors)
 
         return {
+            'objects': objects,
+            'controllers': controllers,
             'sensors': sensors,
         }
 
@@ -58,6 +44,21 @@ class SensorDataResource(BaseResource):
     @schematic_response(SensorDataSchema(many=True))
     def get(self, sensor_id):
         return SensorDataManager(self.db_data).get_all(sensor_id)
+
+
+class AllObjectsInfoResource(BaseResource):
+    @provide_db_session
+    @schematic_response(ResourceSchema())
+    def get(self):
+        objects = ObjectManager(self.db_session).get_all()
+        sensors = SensorManager(self.db_session).get_all()
+        controllers = ControllerManager(self.db_session).get_all()
+
+        return {
+            'objects': objects,
+            'sensors': sensors,
+            'controllers': controllers
+        }
 
 
 def register_routes(app):
