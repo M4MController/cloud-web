ef39d44345ca5edda27d4c3b9498cdcd4798cc7c
Alex Yermakov <aermakov@sgsdt.com>
2020-04-19 03:37:30 +0100

    Flask json validation

--- server/routing.py
+++ server/routing.py
@@ -2,6 +2,7 @@ import bcrypt
 from config import config
 from flask import request
 from flask_jwt_extended import jwt_required, create_access_token
+from flask_expects_json import expects_json
 from server.resources.base import BaseResource
 from server.database.schemas import SensorDataSchema, ResourceSchema
 
@@ -9,112 +10,114 @@ from server.resources.utils import provide_db_session, schematic_response
 
 from server.database.managers import SensorManager, SensorDataManager, ObjectManager, ControllerManager, UserManager
 
+from server.validation.registration import schema as registration_schema
+from server.validation.auth import schema as auth_schema
+
 
 class Registration(BaseResource):
-    @provide_db_session
-    def post(self):
-        login = request.json['email']
-        pwd = request.json['password'].encode('utf-8')
+	@provide_db_session
+	@expects_json(registration_schema)
+	def post(self):
+		login = request.json['email']
+		pwd = request.json['password'].encode('utf-8')
 
-        pwd_hash = bcrypt.hashpw(pwd, bcrypt.gensalt()).decode('utf-8')
-        UserManager(self.db_session).save_new(login, pwd_hash)
+		pwd_hash = bcrypt.hashpw(pwd, bcrypt.gensalt()).decode('utf-8')
+		UserManager(self.db_session).save_new(login, pwd_hash)
 
-        return {'token': create_access_token(identity={'email': login})}
+		return {'token': create_access_token(identity={'email': login})}
 
 
 class Auth(BaseResource):
-    @provide_db_session
-    def post(self):
-        login = request.json['email']
-        pwd = request.json['password'].encode('utf-8')
+	@provide_db_session
+	@expects_json(auth_schema)
+	def post(self):
+		login = request.json['email']
+		pwd = request.json['password'].encode('utf-8')
 
-        hash = UserManager(self.db_session).get_by_login(login).pwd_hash.encode('utf-8')
+		hash = UserManager(self.db_session).get_by_login(login).pwd_hash.encode('utf-8')
 
-        print(hash)
+		if bcrypt.checkpw(pwd, hash):
+			return {'token': create_access_token(identity={'email': login})}
 
-        if bcrypt.checkpw(pwd, hash):
-            return {'token': create_access_token(identity={'email': login})}
-
-        raise ValueError('Incorrect password!')
+		raise ValueError('Incorrect password!')
 
 
 class User(BaseResource):
-    @jwt_required
-    def get(self):
-        return config['user_info']
+	@jwt_required
+	def get(self):
+		return config['user_info']
 
 
 class Users(BaseResource):
-    @provide_db_session
-    def get(self):
-        return UserManager(self.db_session).get_all()
+	@provide_db_session
+	def get(self):
+		return UserManager(self.db_session).get_all()
 
 
 class ObjectsResource(BaseResource):
-    def _insert_last_value(self, sensors):
-        data_manager = SensorDataManager(self.db_session)
+	def _insert_last_value(self, sensors):
+		data_manager = SensorDataManager(self.db_session)
 
-        for sensor in sensors:
-            last_value = data_manager.get_last_record(sensor.id)
-            sensor.last_value = last_value and last_value['value']
-            print(sensor.last_value)
+		for sensor in sensors:
+			last_value = data_manager.get_last_record(sensor.id)
+			sensor.last_value = last_value and last_value['value']
 
-    @jwt_required
-    @provide_db_session
-    @schematic_response(ResourceSchema())
-    def get(self):
-        sensors = SensorManager(self.db_session).get_all()
-        objects = ObjectManager(self.db_session).get_all()
-        controllers = ControllerManager(self.db_session).get_all()
-        self._insert_last_value(sensors)
+	@jwt_required
+	@provide_db_session
+	@schematic_response(ResourceSchema())
+	def get(self):
+		sensors = SensorManager(self.db_session).get_all()
+		objects = ObjectManager(self.db_session).get_all()
+		controllers = ControllerManager(self.db_session).get_all()
+		self._insert_last_value(sensors)
 
-        return {
-            'objects': objects,
-            'controllers': controllers,
-            'sensors': sensors,
-        }
+		return {
+			'objects': objects,
+			'controllers': controllers,
+			'sensors': sensors,
+		}
 
 
 class SensorDataResource(BaseResource):
-    @jwt_required
-    @provide_db_session
-    @schematic_response(SensorDataSchema(many=True))
-    def get(self, sensor_id):
-        result = SensorDataManager(self.db_session)\
-            .get_sensor_data(sensor_id, request.args.get('from'), request.args.get('field'))
+	@jwt_required
+	@provide_db_session
+	@schematic_response(SensorDataSchema(many=True))
+	def get(self, sensor_id):
+		result = SensorDataManager(self.db_session)\
+			.get_sensor_data(sensor_id, request.args.get('from'), request.args.get('field'))
 
-        return result
+		return result
 
 
 class AllObjectsInfoResource(BaseResource):
-    @jwt_required
-    @provide_db_session
-    @schematic_response(ResourceSchema())
-    def get(self):
-        objects = ObjectManager(self.db_session).get_all()
-        sensors = SensorManager(self.db_session).get_all()
-        controllers = ControllerManager(self.db_session).get_all()
+	@jwt_required
+	@provide_db_session
+	@schematic_response(ResourceSchema())
+	def get(self):
+		objects = ObjectManager(self.db_session).get_all()
+		sensors = SensorManager(self.db_session).get_all()
+		controllers = ControllerManager(self.db_session).get_all()
 
-        return {
-            'objects': objects,
-            'sensors': sensors,
-            'controllers': controllers
-        }
+		return {
+			'objects': objects,
+			'sensors': sensors,
+			'controllers': controllers
+		}
 
 
 class SensorDataPrivateResource(BaseResource):
-    @jwt_required
-    @provide_db_session
-    @schematic_response(SensorDataSchema())
-    def post(self, sensor_id):
-        return SensorDataManager(self.db_session).save_new(sensor_id, request.json['sensor_data'])
+	@jwt_required
+	@provide_db_session
+	@schematic_response(SensorDataSchema())
+	def post(self, sensor_id):
+		return SensorDataManager(self.db_session).save_new(sensor_id, request.json['sensor_data'])
 
 
 def register_routes(app):
-    app.register_route(Auth, 'sign_in', '/sign_in')
-    app.register_route(Registration, 'sign_up', '/sign_up')
-    app.register_route(ObjectsResource, 'objects', '/objects')
-    app.register_route(SensorDataResource, 'sensor_data', '/sensor/<int:sensor_id>/data')
-    app.register_route(SensorDataResource, 'sensor_data_private', '/private/sensor/<int:sensor_id>/data/add')
-    app.register_route(User, 'user_info', '/user/info')
-    app.register_route(Users, 'users_list', '/user/list')
+	app.register_route(Auth, 'sign_in', '/sign_in')
+	app.register_route(Registration, 'sign_up', '/sign_up')
+	app.register_route(ObjectsResource, 'objects', '/objects')
+	app.register_route(SensorDataResource, 'sensor_data', '/sensor/<int:sensor_id>/data')
+	app.register_route(SensorDataResource, 'sensor_data_private', '/private/sensor/<int:sensor_id>/data/add')
+	app.register_route(User, 'user_info', '/user/info')
+	app.register_route(Users, 'users_list', '/user/list')
---
+++ server/validation/auth.py
@@ -0,0 +1,12 @@
+schema = {
+	'type': 'object',
+	'properties': {
+		'email': {
+			'type': 'string'
+		},
+		'password': {
+			'type': 'string',
+		},
+	},
+	'required': ['email', 'password'],
+}
